78195154f1035386d6c92da11a8f9372ba656733
cleytonmsouza <60261885+cleytonmsouza@users.noreply.github.com>
2022-02-27 00:29:03 -0300

    Add Abre e Fechar Menu e .JS

---
+++ main.js
@@ -0,0 +1,21 @@
+  
+  /* Abrir e Fechar menu */
+  const nav = document.querySelector('#header nav')
+   const toggle = document.querySelectorAll('nav .toggle')
+
+   for (const element of toggle){
+        element.addEventListener('click', function() {
+            nav.classList.toggle('show')
+          })   
+        }
+
+  /* Quando clicar em item, esconder menu */
+
+  const links = document.querySelectorAll('nav ul li a')
+
+  for ( const link of links) {
+        link.addEventListener('click', function ( ) {
+                nav.classList.remove('show')  
+      })
+  }
+
